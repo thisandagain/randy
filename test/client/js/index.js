e4279eb7b9fbe005682cb5413c79043e965c5fe1
Andrew Sliwinski <thisandagain@gmail.com>
2012-08-13 00:10:16 -0700

    Simplified user registration method (unify type and id)

--- test/client/js/index.js
+++ test/client/js/index.js
@@ -17,20 +17,15 @@ $(document).ready(function() {
     /**
      * Generate unique (random) user id
      */
-    var userId      = Math.floor(Math.random()*99999);
-    var userType    = 'test';
-    var userUnique  = userType + '::' + userId;
-    $user.html(userUnique);
-    $('#target').val(userUnique);
+    var user        = 'test::' + Math.floor(Math.random()*99999);
+    $user.html(user);
+    $('#target').val(user);
 
     /**
      * Socket.io events
      */
     var socket = io.connect('//localhost:80');
-    socket.emit('register', {
-        id: userId,
-        type: userType
-    });
+    socket.emit('register', user);
 
     socket.on('notice', function (data) {
         $activity.prepend('<p>' + JSON.stringify(data) + '</p>');
