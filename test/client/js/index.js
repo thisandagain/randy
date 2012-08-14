dd9da6fc3820f4535546f1a1d42b17079bb30510
Andrew Sliwinski <thisandagain@gmail.com>
2012-08-14 16:37:35 -0700

    Added support for "read" action

--- test/client/js/index.js
+++ test/client/js/index.js
@@ -27,13 +27,18 @@ $(document).ready(function() {
     /**
      * Socket.io events
      */
-    //var socket = io.connect('//localhost:80');
-    var socket = io.connect('//randy.jit.su:80');
+    var socket = io.connect('//localhost:80');
+    //var socket = io.connect('//randy.jit.su:80');
     socket.emit('register', user);
 
     socket.on('notice', function (data) {
         $activity.prepend('<p>' + JSON.stringify(data) + '</p>');
         $activity.find('p').first().prepend('<a href="#" class="dismiss" id="' + data.id + '">Dismiss</a>&nbsp;');
+        $activity.find('p').first().prepend('<a href="#" class="read" id="' + data.id + '">Read</a>&nbsp;');
+    
+        if (data.read) {
+            $activity.find('p').css('opacity', '0.5');
+        }
     });
 
     /**
@@ -47,6 +52,17 @@ $(document).ready(function() {
             target:     $('#target').val(),
             message:    $('#message').val()
         });
+    });
+
+    $('a.read').live('click', function (e) {
+        e.preventDefault();
+
+        socket.emit('read', {
+            id:     $(this).attr('id'),
+            uid:    user
+        });
+
+        $(this).parent().css('opacity', '0.5');
     });
 
     $('a.dismiss').live('click', function (e) {
