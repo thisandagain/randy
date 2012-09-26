cb8b04bc99f9beb6cbbaf136d5a78d6302c5a21f
Andrew Sliwinski <thisandagain@gmail.com>
2012-09-25 17:55:26 -0700

    Migrated randy core to Rodeo

--- test/index.js
+++ test/index.js
@@ -22,12 +22,6 @@ async.auto({
         test("Component definition", function (t) {
             t.type(target, "object", "Component should be an object");
             t.type(target.listen, "function", "Method should be a function");
-            t.type(target.submit, "function", "Method should be a function");
-            t.type(target.destroy, "function", "Method should be a function");
-
-            t.type(target.party, "function", "Method should be a function");
-            t.type(target.beerme, "function", "Method should be a function");
-            t.type(target.passout, "function", "Method should be a function");
             t.end();
         });
 
